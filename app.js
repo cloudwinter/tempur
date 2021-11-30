9889d249757466a805cedb60e569d7ae29e8835d
xiayundong <1032176134@qq.com>
2021-11-30 22:46:50 +0800

    1.0.1-1
    上传首页

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
-// app.js
+//app.js
+const configManager = require('./utils/configManager')
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  onLaunch: function () {
+    configManager.init();
+    // configManager.setSkin('dark');
+    //var skin = configManager.getSkin();
+    this.globalData.skin = 'dark';
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+  },
+  onShow: function () {
+    wx.getSystemInfo({
+      success: (res) => {
+        console.info("app onShow->当前的设备信息：", res);
+        // 设备品
+        var brand = res.brand;
+        var screenHeight = res.screenHeight;
+        var screenWidth = res.screenWidth;
+        var display = 'normal';
+        if (brand.toLowerCase().indexOf('huawei') >= 0 ||
+          brand.toLowerCase().indexOf('vivo') >= 0 ||
+          brand.toLowerCase().indexOf('oppo') >= 0) {
+          display = screenHeight > 690 ? 'high' : 'normal';
+        } else {
+          display = screenHeight > 790 ? 'high' : 'normal';
+        }
+        this.globalData.display = display
+        this.globalData.screenHeight = screenHeight;
+        this.globalData.screenWidth = screenWidth;
+        console.info("app onShow->屏幕高度宽度：" + screenHeight, this.globalData.display, screenWidth);
+      },
     })
   },
   globalData: {
-    userInfo: null
+    skin: 'dark',
+    display: 'normal',
+    screenHeight: '',
+    screenWidth: '',
+    navHeight: '',
+    connected: {}, // 当前已连接的设备信息
+    hasSleepInduction: false, // 智能睡眠感应开关
+    zhinengShuimian:'00', // 智能睡眠状态
+    sleepTimer: '00', // 智能睡眠定时
+    sleepInduction: { //智能睡眠感应信息
+      status: '00', // 00 关闭，01开启 其他定时
+      nightLight: '00', // 智能夜灯 00 关闭 01开启
+      mode: '00', // 模式 00 预设位置 01 个性位置
+      gexingModel: '00' // 个性模式 00 个性未设置 01 个性已设置
+    }
+
   }
 })
